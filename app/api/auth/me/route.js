a7897c0282e1a020773788adcce30658114435d0
alexeysimashkin <lifegood_official@vk.com>
2026-07-27 10:13:07 +0400

    Update route.js

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -1,7 +1,7 @@
-import { verifyToken } from '@/lib/auth';
+import { verifyToken } from '../../../lib/auth';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
-import { query } from '@/lib/db';
+import { query } from '../../../lib/db';
 
 export async function GET() {
   const cookieStore = cookies();
