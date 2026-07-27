037376fc1a942b4c7af5a7b7d479770ef2d49031
alexeysimashkin <lifegood_official@vk.com>
2026-07-27 12:37:16 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,3 @@
-export const metadata = {
-  title: 'Medical MIS',
-  description: 'Medical Information System',
-};
-
 export default function RootLayout({ children }) {
-  return (
-    <html lang="ru">
-      <body>{children}</body>
-    </html>
-  );
+  return children;
 }
